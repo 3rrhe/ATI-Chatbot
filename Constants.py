594bf9c009b60d7f2e53a8bb9b41ae86c3ebd954
3rrhe <rroca@praga.ws>
2022-06-08 04:01:59 -0600

    Added constants security filter

--- Constants.py
+++ Constants.py
@@ -1,8 +1,8 @@
 '''
-The ConstantsLocal file is a file similar to this but if you import it at the beginning,
+The ConstantsEnvLocal file is a file similar to this but if you import it at the beginning,
 the constants are the same and the content is placed in quotes, for example API_KEY='your_bot_token'
 '''
-import ConstantsLocal as keys
+import ConstantsEnvLocal as keys
 
 API_KEY = keys.API_KEY
 ENV = keys.ENV  # This can be 'dev' or 'prod'
