34c414119e875483d2341fb0ecfdfe081970c6ce
3rrhe <rroca@praga.ws>
2022-06-08 03:55:53 -0600

    Added constants security filter

--- Constants.py
+++ Constants.py
@@ -1,1 +1,8 @@
-API_KEY = '5490910176:AAEESWseEs433-gaYoDp9XdjF2yMS6t71hQ'
+'''
+The ConstantsLocal file is a file similar to this but if you import it at the beginning,
+the constants are the same and the content is placed in quotes, for example API_KEY='your_bot_token'
+'''
+import ConstantsLocal as keys
+
+API_KEY = keys.API_KEY
+ENV = keys.ENV  # This can be 'dev' or 'prod'
--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import Constants as keys
 from telegram.ext import *
 import Responsers as R
 
-print("Bot iniciado perras...")
+print("Bot iniciado...")
 
 
 def start_command(update, context):
+    if keys.ENV == 'dev':
+        print("comando start ejecutado")
+
     update.message.reply_text("Escribe algo para que te ayude prro")
 
 
 def help_command(update, context):
-    update.message.reply_text("Mijo googlee, no quiere que le resuelvan todo")
+    if keys.ENV == 'dev':
+        print("comando help ejecutado")
+
+    update.message.reply_text("Mijo googlee, no quiera que le resuelvan todo")
 
 
 def handle_message(update, context):
+    if keys.ENV == 'dev':
+        print("Se ha recibido un mensaje")
+
     text = str(update.message.text).lower()
     response = R.sample_responses(text)
 
@@ -35,7 +44,8 @@ def main():
 
     dp.add_error_handler(error)
 
-    updater.start_polling(60)  # 60 seconds
+    # If you want the bot to have a response time, add a number in seconds inside the parenthesis.
+    updater.start_polling()
     updater.idle()
 
 
